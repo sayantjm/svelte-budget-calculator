09b99d8a7350fcb11f7c4a351e52750305f01dd0
Juanma Perales <sayantjm@gmail.com>
2020-12-27 11:34:00 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	// components
+	import NavBar from './NavBar.svelte';
+	import Title from './Title.svelte';
+	import ExpensesList from './ExpensesList.svelte';
+
+	// data
+	import expensesData from './expenses';
+
+	// variables
+	let expenses = [...expensesData];
+	// functions
+	function removeExpense(id) {
+		expenses = expenses.filter(item => item.id !== id);
+	}
+</script>
+
+<!-- <style></style> -->
+<!-- CSS/STYLING -->
+
+<NavBar />
+<main class="content">
+	<ExpensesList {expenses} {removeExpense}/>
+</main>
---
+++ src/Expense.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let id;
+    export let name = '';
+    export let amount = 0;
+    let displayAmount = false;
+    export let removeExpense;
+
+    function toggleAmount () {
+        displayAmount = !displayAmount;
+    }
+</script>
+
+<article class="single-expense">
+    <div class="expense-info">
+        <h2>
+            {name}
+            <button class="amount-btn" on:click="{toggleAmount}">
+                <i class="fas fa-caret-down"></i>
+            </button>
+        </h2>
+        {#if displayAmount}
+            <h4>amount: ${amount}</h4>    
+        {/if}
+    </div>
+    <div class="expense-buttons">
+        <button class="expense-btn edit-btn">
+            <i class="fas fa-pen"></i>
+        </button>
+        <button class="expense-btn delete-btn" on:click={() => removeExpense(id)}>
+            <i class="fas fa-trash"></i>
+        </button>
+    </div>
+</article>
