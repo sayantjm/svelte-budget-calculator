5fc73f4e2b66012669f4d4d112a3690eb08a85c4
Juanma Perales <sayantjm@gmail.com>
2020-12-28 08:17:30 +0100

    Adding expenses total. Closes #3

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,19 @@
 	import NavBar from './NavBar.svelte';
 	import Title from './Title.svelte';
 	import ExpensesList from './ExpensesList.svelte';
+	import Totals from './Totals.svelte';
 
 	// data
 	import expensesData from './expenses';
 
 	// variables
 	let expenses = [...expensesData];
+
+	// reactive
+	$: total = expenses.reduce((acc, curr) => {
+		return (acc += curr.amount)
+	}, 0);
+
 	// functions
 	function removeExpense(id) {
 		expenses = expenses.filter(item => item.id !== id);
@@ -26,6 +33,7 @@
 
 <NavBar />
 <main class="content">
+	<Totals title="total expenses" {total} />
 	<ExpensesList {expenses} {removeExpense}/>
 	<button type="button" class="btn btn-primary btn-block" on:click={clearExpenses}>
 		clear expenses
