f52e83fbc94c9e194d244b6df2bc2528f0bd4716
Juanma Perales <sayantjm@gmail.com>
2020-12-28 09:04:51 +0100

    Adding Expenses Form and the addExpense funtionality

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
 	import Title from './Title.svelte';
 	import ExpensesList from './ExpensesList.svelte';
 	import Totals from './Totals.svelte';
+	import ExpenseForm from './ExpenseForm.svelte';
 
 	// data
 	import expensesData from './expenses';
@@ -24,6 +25,16 @@
 	function clearExpenses() {
 		expenses=[];
 	}
+	function addExpense({name, amount}) {
+		let expense = {
+			id:Math.random() * Date.now(),
+			name,
+			amount
+		};
+		expenses = [expense, ...expenses];
+	}
+
+
 	// context
 	setContext('remove', removeExpense);
 </script>
@@ -33,6 +44,7 @@
 
 <NavBar />
 <main class="content">
+	<ExpenseForm {addExpense}/>
 	<Totals title="total expenses" {total} />
 	<ExpensesList {expenses} {removeExpense}/>
 	<button type="button" class="btn btn-primary btn-block" on:click={clearExpenses}>
---
+++ src/ExpenseForm.svelte
@@ -0,0 +1,42 @@
+<script>
+    import Title from './Title.svelte';
+    let name = '';
+    let amount = null;
+    export let addExpense;
+
+    $: isEmpty = !name || !amount;
+
+    function handleSubmit() {
+        addExpense({name, amount});
+
+        name = '';
+        amount = null;
+    }
+</script>
+
+<section class="form">
+    <Title title="add expense" />
+    <form class="expense-form" on:submit|preventDefault={handleSubmit}>
+        <div class="form-control">
+            <label for="name">name</label>
+            <input type="text" id="name" bind:value={name}>
+        </div>
+        <div class="form-control">
+            <label for="amount">amount</label>
+            <input type="number" id="amount" bind:value={amount}>
+        </div>
+        {#if isEmpty}
+            <p class="form-empty">please fill out all form fields</p>
+        {/if}
+        <button type="submit" 
+                class="btn btn-block"
+                class:disabled={isEmpty} 
+                disabled={isEmpty}>
+            add expense
+        </button>
+        <button type="button" class="close-btn">
+            close
+            <i class="fas fa-times"></i>
+        </button>
+    </form>
+</section>
